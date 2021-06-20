59d38743cd515f48b6d8eb86796fb887dca1d293
emreguler <emre_guler01@hotmail.com>
2021-06-20 18:05:51 +0300

    JWT Create added.

--- Vega/Services/JwtService.cs
+++ Vega/Services/JwtService.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Vega.Data;
 using Vega.Entities;
 using Vega.Interfaces;
@@ -7,14 +14,35 @@ namespace Vega.Services
     public class JwtService : IJwtService
     {
         private readonly VegaContext _db;
-        public JwtService(VegaContext db)
+        private IConfiguration Configuration { get; }
+        public JwtService(VegaContext db, IConfiguration configuration)
         {
             _db = db;
+            Configuration = configuration;
         }
 
         public string Create (User user)
         {
-            return "";
+            SymmetricSecurityKey secureKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecureKey"]));
+            SigningCredentials credentials = new SigningCredentials(secureKey, SecurityAlgorithms.HmacSha256);
+            
+            List<Claim> claims = new() 
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Role.ToString())
+            };
+
+            JwtSecurityToken token =  new JwtSecurityToken (
+                issuer: Configuration["Jwt:SecureKey"],
+                audience: Configuration["Jwt:SecureKey"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: credentials
+            );
+
+            string encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            
+            return encodedToken;
         }
     }
 }
